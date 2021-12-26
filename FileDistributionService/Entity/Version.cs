901cdf469e6ef4060d78bb72fea61fa64bb7dfd6
StefanTrpkovic <stefan.trpkovik@gmail.com>
2021-12-27 00:27:26 +0100

    //

--- FileDistributionService/Entity/Version.cs
+++ FileDistributionService/Entity/Version.cs
@@ -3,7 +3,7 @@
     public class Version
     {
         public int Id { get; set; }
-        public string Version { get; set; }
+        public string VersionCode { get; set; }
         public ICollection<SoftwareVersion> SoftwareVersions { get; set; }
 
     }
