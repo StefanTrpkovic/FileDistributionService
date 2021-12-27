87713fd6e73cc53e5db5fe30a9aaac27351cadef
StefanTrpkovic <stefan.trpkovik@gmail.com>
2021-12-27 01:21:27 +0100

    Added JWT token authentication

---
+++ FileDistributionService/Controllers/AccountController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using FileDistributionService.Models;
+
+namespace FileDistributionService.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly JwtSettings jwtSettings;
+        public AccountController(JwtSettings jwtSettings)
+        {
+            this.jwtSettings = jwtSettings;
+        }
+        private IEnumerable<Users> logins = new List<Users>() {
+            new Users() {
+                    Id = Guid.NewGuid(),
+                        EmailId = "stefan.trpkovic@gmail.com",
+                        UserName = "admin",
+                        Password = "admin",
+                },
+                new Users() {
+                    Id = Guid.NewGuid(),
+                        EmailId = "anders.hansen@gmail.com",
+                        UserName = "anders",
+                        Password = "anders1",
+                },
+                new Users() {
+                    Id = Guid.NewGuid(),
+                        EmailId = "fabricio.bianchi@gmail.com",
+                        UserName = "fabricio",
+                        Password = "fabi1",
+                },
+                new Users() {
+                    Id = Guid.NewGuid(),
+                        EmailId = "andy.smith@gmail.com",
+                        UserName = "andy",
+                        Password = "andy1",
+                },
+                new Users() {
+                    Id = Guid.NewGuid(),
+                        EmailId = "jan.vrba@gmail.com",
+                        UserName = "jan",
+                        Password = "jan1",
+                },
+                new Users() {
+                    Id = Guid.NewGuid(),
+                        EmailId = "alex.sidorov@gmail.com",
+                        UserName = "alex",
+                        Password = "alex1",
+                }
+        };
+        [HttpPost]
+        public IActionResult GetToken(UserLogins userLogins)
+        {
+            try
+            {
+                var Token = new UserTokens();
+                var Valid = logins.Any(x => x.UserName.Equals(userLogins.UserName, StringComparison.OrdinalIgnoreCase));
+                if (Valid)
+                {
+                    var user = logins.FirstOrDefault(x => x.UserName.Equals(userLogins.UserName, StringComparison.OrdinalIgnoreCase));
+                    Token = JwtHelpers.JwtHelpers.GenTokenkey(new UserTokens()
+                    {
+                        EmailId = user.EmailId,
+                        GuidId = Guid.NewGuid(),
+                        UserName = user.UserName,
+                        Id = user.Id,
+                    }, jwtSettings);
+                }
+                else
+                {
+                    return BadRequest($"wrong password");
+                }
+                return Ok(Token);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+        /// <summary>
+        /// Get List of UserAccounts
+        /// </summary>
+        /// <returns>List Of UserAccounts</returns>
+        [HttpGet]
+        [Authorize(AuthenticationSchemes = Microsoft.AspNetCore.Authentication.JwtBearer.JwtBearerDefaults.AuthenticationScheme)]
+        public IActionResult GetList()
+        {
+            return Ok(logins);
+        }
+    }
+}
--- FileDistributionService/Controllers/ClientController.cs
+++ FileDistributionService/Controllers/ClientController.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace FileDistributionService.Controllers
 {
@@ -7,9 +8,15 @@ namespace FileDistributionService.Controllers
     [ApiController]
     public class ClientController : ControllerBase
     {
-        [HttpGet(Name = "GetAvailableUpdates")]
-        public async Task<IActionResult> Get(string packageId, string version)
+        [HttpGet]
+        [Authorize(AuthenticationSchemes = Microsoft.AspNetCore.Authentication.JwtBearer.JwtBearerDefaults.AuthenticationScheme)]
+        public async Task<IActionResult> GetAvailableUpdates(string packageId, string version)
         {
+            var bearerToken = HttpContext.Request.Headers["Authorization"][0];
+            var token = new JwtSecurityTokenHandler().ReadJwtToken(bearerToken.Replace("Bearer ", ""));
+
+            var userEmail = token.Payload.ToArray()[2].Value;
+
             return Ok();
         }
     }
