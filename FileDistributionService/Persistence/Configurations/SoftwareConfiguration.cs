063c163097506ab467f40bcc1281778226c63248
StefanTrpkovic <stefan.trpkovik@gmail.com>
2022-01-25 23:36:16 +0100

    Update db data

--- FileDistributionService/Persistence/Configurations/SoftwareConfiguration.cs
+++ FileDistributionService/Persistence/Configurations/SoftwareConfiguration.cs
@@ -14,7 +14,7 @@ namespace Persistence.Configurations
                 new Software { Id = 1, Name = "Visual Studio", PackageId = "A0B4NCJ6", ChannelId = 1, Version = 1, ReleaseDate = new DateTime(2017, 5, 12, 0, 0, 0, 0) }, 
                 new Software { Id = 2, Name = "Visual Studio Code", PackageId = "K65FB9D4", ChannelId = 2, Version = 2, ReleaseDate = new DateTime(2017, 5, 12, 0, 0, 0, 0)}, 
                 new Software { Id = 3, Name = "Mongo DB Compass", PackageId = "P2F86NTC", ChannelId = 3, Version = 3, ReleaseDate = new DateTime(2017, 5, 12, 0, 0, 0, 0) }, 
-                new Software { Id = 4, Name = "Visual Studio", PackageId = "FN51MQ6G", ChannelId = 2, Version = 4, ReleaseDate = new DateTime(2017, 5, 12, 0, 0, 0, 0)}, 
+                new Software { Id = 4, Name = "Visual Studio", PackageId = "FN51MQ6G", ChannelId = 2, Version = 2, ReleaseDate = new DateTime(2017, 5, 12, 0, 0, 0, 0)}, 
                 new Software { Id = 5, Name = "Ditto pasting tool", PackageId = "MY49C6KL", ChannelId = 1, Version = 1, ReleaseDate = new DateTime(2017, 5, 12, 0, 0, 0, 0)}
             );
         }
